41df6d57ea414569d8d048db6a60dbf287359020
andre.figueiredo <andre.figueiredo@miniclip.com>
2023-05-29 12:44:26 +0100

    Initial Commit

---
+++ FastlaneSample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FastlaneSample
+//
+//  Created by André Figueiredo on 29/05/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
